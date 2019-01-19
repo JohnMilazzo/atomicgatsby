d0f2aac38729a7e00c61a3bc4ab017aa9aa49a3a
John Milazzo <github@johnmilazzo.com>
2019-01-18 23:13:21 -0600

    added google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default () => (
   <>
     <h1>Atomic Gatsby</h1>
     <h2>Home of the Gatsby Solos</h2>
+    <ul>
+      <li>
+        <Link to="/solos/gatsby-solo">gatsby-solo</Link>
+      </li>
+      <li>
+        <Link to="/solos/gatsby-solo-helmet">gatsby-solo-helmet</Link>
+      </li>
+      <li>
+        <Link to="/solos/gatsby-solo-typography">gatsby-solo-typography</Link>
+      </li>
+    </ul>
   </>
 );
