78817fc2b7574bcaf352d5b99252bd57b58069c2
John Milazzo <github@johnmilazzo.com>
2019-01-20 22:50:02 -0600

    added Helmet to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { Link } from 'gatsby';
 
 export default () => (
   <>
+    <Helmet>
+      <html lang="en" />
+      <title>Atomic Gatsby</title>
+      <meta name="description" content="Home of the Gatsby Solos" />
+    </Helmet>
     <h1>Atomic Gatsby</h1>
     <h2>Home of the Gatsby Solos</h2>
     <ul>
